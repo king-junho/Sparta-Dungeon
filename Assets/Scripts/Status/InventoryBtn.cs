1b33b7aa5d456aad1fe896d0824ae13cb21b78c4
king-junho <junho087387@gmail.com>
2023-12-12 20:13:03 +0900

    [ADD} STATUS UI
    스테이터스 UI 및 버튼 기능 구현

---
+++ Assets/Scripts/Status/InventoryBtn.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryBtn : MonoBehaviour
+{
+    public void PressedInventoryBtn()
+    {
+        Debug.Log("인벤토리 버튼 눌림");
+    }
+}
